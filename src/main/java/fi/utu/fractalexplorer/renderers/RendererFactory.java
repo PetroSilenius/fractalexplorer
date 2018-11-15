20ecb10cbb8874549c8a4a11f4d4b486a6db06fc
Jari-Matti Mäkelä <jmjm@iki.fi>
2018-11-15 16:55:56 +0200

    -

--- src/main/java/fi/utu/fractalexplorer/renderers/RendererFactory.java
+++ src/main/java/fi/utu/fractalexplorer/renderers/RendererFactory.java
@@ -1,37 +1,11 @@
 package fi.utu.fractalexplorer.renderers;
 
-import fi.utu.fractalexplorer.kernels.MandelbrotFastKernel;
-import fi.utu.fractalexplorer.kernels.MandelbrotSlowKernel;
-
 /**
  * You need to consider if this needs to be changed in order to support all rendering modes.
  */
 public class RendererFactory {
     public static MandelbrotRenderer createRenderer(RendererType type, int w, int h, int maxIterations, PixelRenderer p) throws Exception {
 
-        class FastRenderer extends RenderBase implements MandelbrotFastKernel, MandelbrotSequentialRenderer {
-            public FastRenderer(int w, int h, int maxIterations, PixelRenderer p) {
-                super(w, h, maxIterations, p);
-            }
-        }
-
-        class SlowRenderer extends RenderBase implements MandelbrotSlowKernel, MandelbrotSequentialRenderer {
-            public SlowRenderer(int w, int h, int maxIterations, PixelRenderer p) {
-                super(w, h, maxIterations, p);
-            }
-        }
-
-        class StaticThreadRenderer extends RenderBase implements MandelbrotFastKernel, MandelbrotStaticParallelRenderer {
-            public StaticThreadRenderer(int w, int h, int maxIterations, PixelRenderer p) {
-                super(w, h, maxIterations, p);
-            }
-        }
-
-        class DynThreadRenderer extends RenderBase implements MandelbrotFastKernel, MandelbrotDynamicParallelRenderer {
-            public DynThreadRenderer(int w, int h, int maxIterations, PixelRenderer p) {
-                super(w, h, maxIterations, p);
-            }
-        }
 
         switch (type) {
             case Slow:
