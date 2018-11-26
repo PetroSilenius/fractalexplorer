dadd20d354f45bbaa4efa2afa80a5c59e75969be
Jari-Matti Mäkelä <jmjm@iki.fi>
2018-11-26 17:25:09 +0200

    Fix float imprecisions in calculations + documentation

--- src/main/java/fi/utu/fractalexplorer/renderers/MandelbrotRenderer.java
+++ src/main/java/fi/utu/fractalexplorer/renderers/MandelbrotRenderer.java
@@ -13,34 +13,55 @@ public interface MandelbrotRenderer extends MandelbrotKernel {
 
     PixelRenderer pixelRenderer();
 
-    // draw a tile
+    /**
+     * Draw a region of the Mandelbrot set. The pixel coordinates
+     * (inclusive) of the region are (tx, ty) - (tx+tw-1, ty+th-1).
+     *
+     * Due to the vector processing, tw % vectorSize() == 0 must be
+     * always true!
+     *
+     * Params:
+     *  (tx, ty) - upper left coordinate (inclusive)
+     *  (tw, th) - tile width & height
+     *  vp       - viewport settings
+     *
+     */
     default void drawTile(int tx, int ty, int tw, int th, Viewport vp) {
         final double w = renderWidth(), h = renderHeight();
         final int ww = renderWidth();
-        final double zoom = vp.zoom / Math.min(w, h) * 2;
-        final double zoom2 = 2 * zoom;
+        final int v = vectorSize();
+
+        // tile bottom-right coordinates (exclusive)
         final int mx = tx + tw;
         final int my = ty + th;
+
+        // viewport parameters
+        final double zoom = vp.zoom / Math.min(w, h) * 2;
+        final double zoom2 = 2 * zoom;
         final double s = Math.sin(vp.rot);
         final double c = Math.cos(vp.rot);
         final double vpc = vp.dx - c;
         final double vps = vp.dy + s;
 
-        double x = tx;
-        double y = (2*ty - h) * zoom;
+        double x, sx = - w * zoom;
+        double y = - h * zoom;
+        for (int i=0;i<ty;i++) y  += zoom2; // emulate float imprecision
+        for (int i=0;i<tx;i++) sx += zoom2; // emulate float imprecision
+
         int lineIdx = ty * ww;
 
         for (int yy = ty; yy < my; yy++) {
-            x = (2*tx - w) * zoom;
-            for (int xx = tx; xx < mx; xx += vectorSize()) {
-                if (vectorSize() > 1) {
+            x = sx;
+
+            for (int xx = tx; xx < mx; xx += v) {
+                if (v > 1) {
 
                     int[] iterValues = mandelbrot(vpc + x, vps + y, zoom2, vp.rot);
 
-                    for (int i = 0; i < vectorSize(); i++)
+                    for (int i = 0; i < v; i++)
                         pixelRenderer().setPixel(lineIdx + xx + i, iterValues[i]);
 
-                    x += zoom2 * vectorSize();
+                    x += zoom2 * v;
                 } else {
                     final double c_re = vpc + x;
                     final double c_im = vps + y;
